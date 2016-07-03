6c7af584f849020677247b123b6bbb348ee769a7
Megaju <julien_malle@outlook.fr>
2016-07-03 15:18:11 +0200

    Ajout bouton poulet + multiplicateur

--- js/game.js
+++ js/game.js
@@ -1,10 +1,22 @@
 var score = 0;
 var eggs = 0;
-//var affichage
-//var bouton
-var multiplicateur = 1;
-var compteur = 1;
+var upgrade = 1;
 
-function incrementer() {
-    
+function clicOnChicken() {
+    score = score + upgrade;
+    eggs = eggs + upgrade;
+    document.getElementById("affichage-score").innerHTML = eggs;
+    document.getElementById("total-eggs").innerHTML = score;
+    console.log(eggs);
+}
+
+function buy() {
+    if (eggs >= 50) {
+        eggs = eggs - 50;
+        document.getElementById("affichage-score").innerHTML = eggs;
+        upgrade = upgrade + 1;
+    } else {
+        alert("nop !");
+    }
+        
 }
